7f4cb4bea8be8da6b283b11f97e07e45893a0d4d
Moses Fabiyi <90117777+officialmofabs@users.noreply.github.com>
2025-04-01 16:23:50 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'workspace-registry',
+    description: 'store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://officialmofabs.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/officialmofabs',
   },
   reactStrictMode: true,
   swcMinify: true,
